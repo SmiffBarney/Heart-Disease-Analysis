beb39dd9c22e6ca131cc30289d6ca2ea15de42e4
Sean <niezsean@gmail.com>
2024-12-18 21:04:26 -0500

    Imputed missing values

--- test_script_1.py
+++
@@ -1,5 +0,0 @@
-from utils.data_preprocessing import load_data, impute_missing_values
-
-# Read in data
-df = load_data("data/framingham.csv")
-
--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -1,13 +1,18 @@
 from sklearn.impute import SimpleImputer
 import pandas as pd
 
-# Load in CSV file
-def load_data(path):
-    return pd.read_csv(path)
+# Read in raw data
+df = pd.read_csv("data/framingham.csv")
 
-# Handle null values
-def impute_missing_values(df, strategy="mean"):
-    imputer = SimpleImputer(strategy=strategy)
-    df_imputed = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
-    return df_imputed
+# Handle missing continuous data
+cont_to_impute = ['glucose', 'cigsPerDay', 'totChol', 'BMI', 'heartRate']
+cont_imputer = SimpleImputer(strategy='median')
+df[cols_to_impute] = cont_imputer.fit_transform(df[cols_to_impute])
 
+# Handle missing ordinal/binary data
+ord_to_impute = ['education', 'BPMeds']
+ord_imputer = SimpleImputer(strategy='most_frequent')
+df[ord_to_impute] = ord_imputer.fit_transform(df[ord_to_impute])
+
+# Output processed dataset
+df.to_csv('data/cleaned_data.csv', index=False)
